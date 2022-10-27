61a33102fdfcc77a92bc60ff7dfe4d9f5bfcc424
Monica Pena <113930652+monica-ap@users.noreply.github.com>
2022-10-27 17:58:43 +0000

    practiced defining and using return instead of print

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 #from methods_help import method_help
-from functions import welcome, add_numbers, num_dividedFrom_add_numbers, greet, square
-#from returnStuff import returnS
+#from functions import welcome, add_numbers, num_dividedFrom_add_numbers, greet, square
+from returnStuff import power, usd_to_eur
 
 
 #method_help()
 #function()
 #returnS()
-added = add_numbers(45,5)
-print(added)
+# added = add_numbers(45,5)
+# print(added)
 
 
-final=num_dividedFrom_add_numbers(45,added)
-print(final)
+# final=num_dividedFrom_add_numbers(45,added)
+# print(final)
 
 
-greet()
-welcome("Monica")
-square(8)
+# greet()
+# welcome("Monica")
+# square(8)
+
+
+exponent=power(2,3)
+print(exponent)
+
+money= usd_to_eur(12)
+print(money)
