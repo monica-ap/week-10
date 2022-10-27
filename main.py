d4687b4592bf16ad546bdb308cb5ca32da97971f
Monica Pena <113930652+monica-ap@users.noreply.github.com>
2022-10-27 17:23:25 +0000

    worked with functions, methods, and return

---
+++ main.py
@@ -0,0 +1,15 @@
+#from methods_help import method_help
+from functions import function, welcome, add_numbers, num_dividedFrom_add_numbers
+#from returnStuff import returnS
+
+
+#method_help()
+function()
+#returnS()
+welcome("Monica")
+added = add_numbers(45,5)
+print(added)
+
+
+final=num_dividedFrom_add_numbers(45,added)
+print(final)
